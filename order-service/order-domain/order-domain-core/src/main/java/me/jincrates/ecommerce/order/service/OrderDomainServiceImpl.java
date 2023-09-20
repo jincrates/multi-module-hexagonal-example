8403bb3afc8b3c659683ef1ed2b61788e68c4791
jincrates <94jingyu@gmail.com>
2023-09-21 00:24:41 +0900

    feat: 로깅 추가, 코드 개선

--- order-service/order-domain/order-domain-core/src/main/java/me/jincrates/ecommerce/order/service/OrderDomainServiceImpl.java
+++ order-service/order-domain/order-domain-core/src/main/java/me/jincrates/ecommerce/order/service/OrderDomainServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import me.jincrates.ecommerce.domain.valueobject.ProductId;
 import me.jincrates.ecommerce.order.domain.entity.Order;
 import me.jincrates.ecommerce.order.domain.entity.Product;
 import me.jincrates.ecommerce.order.domain.entity.Store;
@@ -25,34 +26,34 @@ public class OrderDomainServiceImpl implements OrderDomainService {
         setOrderProductInformation(order, store);
         order.validateOrder();
         order.initializeOrder();
-        //log.info("Order with id: {} is initiated", order.getId().getValue());
+        log.info("Order with id: {} is initiated", order.getId().getValue());
         return new OrderCreatedEvent(order, getCreatedAt());
     }
 
     @Override
     public OrderPaidEvent payOrder(Order order) {
         order.pay();
-        //log.info("Order with id: {} is paid", order.getId().getValue());
+        log.info("Order with id: {} is paid", order.getId().getValue());
         return new OrderPaidEvent(order, getCreatedAt());
     }
 
     @Override
     public void approveOrder(Order order) {
         order.approve();
-        //log.info("Order with id: {} is approved", order.getId().getValue());
+        log.info("Order with id: {} is approved", order.getId().getValue());
     }
 
     @Override
     public OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages) {
         order.initCancel(failureMessages);
-        //log.info("Order payment is cancelling for order id: {}", order.getId().getValue());
+        log.info("Order payment is cancelling for order id: {}", order.getId().getValue());
         return new OrderCancelledEvent(order, getCreatedAt());
     }
 
     @Override
     public void cancelOrder(Order order, List<String> failureMessages) {
         order.cancel(failureMessages);
-        //log.info("Order with id: {} is cancelled", order.getId().getValue());
+        log.info("Order with id: {} is cancelled", order.getId().getValue());
     }
 
     private void validateStore(Store store) {
@@ -63,18 +64,15 @@ public class OrderDomainServiceImpl implements OrderDomainService {
     }
 
     private void setOrderProductInformation(Order order, Store store) {
-        // 매장의 상품 목록을 Map으로 변환합니다. 상품을 키로 사용하고, 상품의 이름과 가격을 값으로 사용합니다.
-        Map<Product, Product> storeProductMap = store.getProducts().stream()
-            .collect(Collectors.toMap(
-                product -> product,
-                product -> product
-            ));
+        // 매장의 상품 목록을 productId를 키로 사용하여 Map으로 변환합니다.
+        Map<ProductId, Product> storeProductMap = store.getProducts().stream()
+                .collect(Collectors.toMap(Product::getId, product -> product));
 
         // 주문 항목의 상품이 매장의 상품 Map에 있는지 확인하고 정보를 업데이트합니다.
         order.getItems().forEach(orderItem -> {
             Product currentProduct = orderItem.getProduct();
-            if (storeProductMap.containsKey(currentProduct)) {
-                Product matchedProduct = storeProductMap.get(currentProduct);
+            if (storeProductMap.containsKey(currentProduct.getId())) {
+                Product matchedProduct = storeProductMap.get(currentProduct.getId());
                 currentProduct.updateWithConfirmedNameAndPrice(matchedProduct.getName(),
                     matchedProduct.getPrice());
             }
