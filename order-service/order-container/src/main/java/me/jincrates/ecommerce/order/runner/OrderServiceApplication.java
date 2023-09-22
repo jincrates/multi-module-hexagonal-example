c62fdedbf53e525e0082b22b815f1574f4367c06
jincrates <94jingyu@gmail.com>
2023-09-22 11:01:37 +0900

    fix: Runner 수정

--- order-service/order-container/src/main/java/me/jincrates/ecommerce/order/runner/OrderServiceApplication.java
+++ order-service/order-container/src/main/java/me/jincrates/ecommerce/order/runner/OrderServiceApplication.java
@@ -1,5 +1,16 @@
 package me.jincrates.ecommerce.order.runner;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = {"me.jincrates.ecommerce.order.dataaccess"})
+@EntityScan(basePackages = {"me.jincrates.ecommerce.order.dataaccess"})
+@SpringBootApplication(scanBasePackages = "me.jincrates.ecommerce")
 public class OrderServiceApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
 }
