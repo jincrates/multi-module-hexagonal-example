df960dd0630fc2ecd58a4ccefabab1aa5862c1c4
jincrates <94jingyu@gmail.com>
2023-09-22 18:11:42 +0900

    feat: mock 데이터 추가

--- order-service/order-dataaccess/src/main/java/me/jincrates/ecommerce/order/dataaccess/customer/adapter/CustomerAdapter.java
+++ order-service/order-dataaccess/src/main/java/me/jincrates/ecommerce/order/dataaccess/customer/adapter/CustomerAdapter.java
@@ -2,6 +2,7 @@ package me.jincrates.ecommerce.order.dataaccess.customer.adapter;
 
 import java.util.Optional;
 import java.util.UUID;
+import me.jincrates.ecommerce.domain.valueobject.CustomerId;
 import me.jincrates.ecommerce.order.domain.entity.Customer;
 import me.jincrates.ecommerce.order.port.output.persistence.CustomerPort;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,11 @@ public class CustomerAdapter implements CustomerPort {
 
     @Override
     public Optional<Customer> findCustomer(UUID customerId) {
-        return Optional.empty();
+        return Optional.of(new Customer(
+            new CustomerId(UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6")),
+            "email",
+            "name"
+        ));
     }
 
     @Override
--- order-service/order-dataaccess/src/main/java/me/jincrates/ecommerce/order/dataaccess/store/StoreAdapter.java
+++ order-service/order-dataaccess/src/main/java/me/jincrates/ecommerce/order/dataaccess/store/StoreAdapter.java
@@ -1,6 +1,13 @@
 package me.jincrates.ecommerce.order.dataaccess.store;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+import me.jincrates.ecommerce.domain.valueobject.Money;
+import me.jincrates.ecommerce.domain.valueobject.ProductId;
+import me.jincrates.ecommerce.domain.valueobject.StoreId;
+import me.jincrates.ecommerce.order.domain.entity.Product;
 import me.jincrates.ecommerce.order.domain.entity.Store;
 import me.jincrates.ecommerce.order.port.output.persistence.StorePort;
 import org.springframework.stereotype.Component;
@@ -10,6 +17,11 @@ public class StoreAdapter implements StorePort {
 
     @Override
     public Optional<Store> findStoreInformation(Store store) {
-        return Optional.empty();
+
+        return Optional.of(new Store(
+            new StoreId(UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6")),
+            List.of(new Product(new ProductId(UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6")), "상품명", new Money(new BigDecimal(1000)))),
+            true
+        ));
     }
 }
