6604e439c249bee76158139adde6ca5b8adb2a29
jincrates <94jingyu@gmail.com>
2023-09-24 16:42:04 +0900

    fix: 오타 수정

--- infra/kafka/kafka-producer/src/main/java/me/jincrates/ecommerce/infra/kafka/producer/KafkaProducerImpl.java
+++ infra/kafka/kafka-producer/src/main/java/me/jincrates/ecommerce/infra/kafka/producer/KafkaProducerImpl.java
@@ -1,8 +1,6 @@
 package me.jincrates.ecommerce.infra.kafka.producer;
 
 import jakarta.annotation.PreDestroy;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.BiConsumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.jincrates.ecommerce.infra.kafka.producer.exception.KafkaProducerException;
@@ -10,6 +8,9 @@ import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
 
 @Slf4j
 @Component
@@ -22,11 +23,11 @@ public class KafkaProducerImpl implements KafkaProducer {
     public void send(String topicName, String key, Object message, BiConsumer<SendResult<String, Object>, Throwable> callback) {
         log.info("Sending message={} to topic={}", message, topicName);
         try {
-            CompletableFuture<SendResult<String, Object>> kafkaResultFutre = kafkaTemplate.send(topicName, key, message);
-            kafkaResultFutre.whenComplete(callback);
+            CompletableFuture<SendResult<String, Object>> kafkaResultFuture = kafkaTemplate.send(topicName, key, message);
+            kafkaResultFuture.whenComplete(callback);
         } catch (KafkaException ex) {
             log.error("Error on kafka producer with key: {}, message: {}, and exception: {}", key, message,
-                ex.getMessage());
+                    ex.getMessage());
             throw new KafkaProducerException("Error on kafka producer with key: " + key + " and message: " + message);
         }
     }
