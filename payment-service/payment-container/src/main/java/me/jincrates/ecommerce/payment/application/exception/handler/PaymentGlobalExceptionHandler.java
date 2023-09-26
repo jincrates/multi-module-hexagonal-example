6359e266fe8d5a4a51a4e081c89162715d0d01ac
jincrates <94jingyu@gmail.com>
2023-09-26 17:51:52 +0900

    feat: payment-service 모듈 추가

---
+++ payment-service/payment-container/src/main/java/me/jincrates/ecommerce/payment/application/exception/handler/PaymentGlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package me.jincrates.ecommerce.payment.application.exception.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import me.jincrates.ecommerce.application.handler.ErrorResponse;
+import me.jincrates.ecommerce.order.domain.exception.OrderDomainException;
+import me.jincrates.ecommerce.order.domain.exception.OrderNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@ControllerAdvice
+public class PaymentGlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = {OrderDomainException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleException(OrderDomainException orderDomainException) {
+        log.error(orderDomainException.getMessage(), orderDomainException);
+        return new ErrorResponse(
+            HttpStatus.BAD_REQUEST.getReasonPhrase(),
+            orderDomainException.getMessage()
+        );
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = {OrderNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleException(OrderNotFoundException orderNotFoundException) {
+        log.error(orderNotFoundException.getMessage(), orderNotFoundException);
+        return new ErrorResponse(
+            HttpStatus.NOT_FOUND.getReasonPhrase(),
+            orderNotFoundException.getMessage()
+        );
+    }
+}
